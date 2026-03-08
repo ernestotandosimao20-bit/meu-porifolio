3759d64b0cfac978ccc98cc4835a1706a8712d34
Ernesto António <ernestotandosimao2.0@gmail.com>
2026-03-08 20:13:49 +0100

    Add files via upload
    Criando meu portfólio

---
+++ script.js
@@ -0,0 +1,36 @@
+// typing effect
+const text = [
+"Estudante de Informática",
+"Desenvolvedor Web",
+"Futuro Hacker Ético",
+"Amante de Automação"
+]
+
+let count = 0
+let index = 0
+let currentText = ""
+let letter = ""
+
+function type(){
+if(count === text.length){
+count = 0
+}
+currentText = text[count]
+letter = currentText.slice(0, ++index)
+document.getElementById("typing").textContent = letter
+if(letter.length === currentText.length){
+count++
+index = 0
+}
+
+setTimeout(type,100)
+}
+type()
+
+// dark mode
+
+const button = document.getElementById("darkmode")
+button.addEventListener("click",()=>{
+  const body=document.body
+  body.classList.toggle("light")
+})
